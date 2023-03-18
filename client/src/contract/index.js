24ed97d5e734504fb47bfdb4c2e7b346abc24b29
0xgrimredstone <0xgrimredstone@gmail.com>
2023-03-18 12:28:00 +0800

    initial commit
    based off the tutorial by JS Mastery
    
    TODO:
    - hide multiplayer function for now
    - redo Room.js
          - GLTF files
          - functionality
    - choosing map (levels)

---
+++ client/src/contract/index.js
@@ -0,0 +1,4 @@
+import contract from './AVAXGods.json';
+
+export const ADDRESS = '0x286a5c03b9F6e8fE94ea632c18675c2eBE971f4D'
+export const { abi: ABI } = contract;
---
+++ readme.md
@@ -0,0 +1,5 @@
+<!-- @format -->
+
+# Liminal Floor - (soon Multiplayer) Web3 NFT Horror Puzzle Game
+
+![Gameplay](./client/src/assets/background/launch-img.jpg)
